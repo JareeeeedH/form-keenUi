08d974cc23d22c5becbb452c119b05991aad7c61
Jared <jared03050@gmail.com>
2021-10-28 00:25:36 +0800

    fm, init the project and try Ui component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <section class="page page--ui-alert">
+        <h2 class="page__title">UiAlert</h2>
+
+        <p>UiAlert shows an inline alert message to the user. Supported types are <code>info</code>, <code>success</code>, <code>warning</code> and <code>error</code>.</p>
+
+        <p>UiAlert supports keyboard navigation, can contain links and can be dismissed. The alert icon can be changed or removed.</p>
+
+        <h3 class="page__section-title">
+            Examples <a href="https://github.com/JosephusPaye/Keen-UI/blob/master/docs-src/pages/UiAlert.vue" target="_blank" rel="noopener">View Source</a>
+        </h3>
+
+        <div class="page__examples">
+            <ui-alert @dismiss="showAlert1 = false" v-show="showAlert1">
+                Hi everybody! This is the default alert.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert2 = false" type="success" v-show="showAlert2">
+                Okilly dokilly, your account was updated successfully.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert3 = false" type="warning" v-show="showAlert3">
+                Ay caramba! Alerts can also contain HTML. <a href="https://google.com" target="_blank" rel="noopener">Click here</a> for Google.com.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert4 = false" type="error" v-show="showAlert4">
+                D'oh! Something went wrong and we cannot process your request at this time. Try again later.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert5 = false" type="warning" v-show="showAlert5">
+                <ui-icon slot="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.984 14.016V9h-1.97v5.016h1.97zm0 3.984v-2.016h-1.97V18h1.97zm2.672-14.016c.75 0 1.36.61 1.36 1.36V20.67c0 .75-.61 1.314-1.36 1.314H8.343c-.75 0-1.36-.563-1.36-1.313V5.344c0-.75.61-1.36 1.36-1.36h1.64V2.016h4.032v1.97h1.64z"/></svg>
+                </ui-icon>
+
+                This alert has a custom icon.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert6 = false" v-show="showAlert6">
+                This is a multi-line alert. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor suscipit facilis explicabo officiis consectetur, ipsam voluptate excepturi quas quae. Dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, autem.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert7 = false" remove-icon v-show="showAlert7">
+                The icon for this alert has been removed.
+            </ui-alert>
+
+            <ui-alert @dismiss="showAlert8 = false" type="warning" disable-animation v-show="showAlert8">
+                Animations for this alert are disabled.
+            </ui-alert>
+
+            <ui-alert :dismissible="false">This alert is not dismissible.</ui-alert>
+
+            <ui-alert :dismissible="false" remove-icon>
+                This alert has no icon is not dismissible.
+            </ui-alert>
+
+            <ui-button @click="resetAlerts">Reset Alerts</ui-button>
+        </div>
+
+    </section>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
+export default {
+    data() {
+        return {
+            showAlert1: true,
+            showAlert2: true,
+            showAlert3: true,
+            showAlert4: true,
+            showAlert5: true,
+            showAlert6: true,
+            showAlert7: true,
+            showAlert8: true
+        };
+    },
+
+    methods: {
+        resetAlerts() {
+            this.showAlert1 = true;
+            this.showAlert2 = true;
+            this.showAlert3 = true;
+            this.showAlert4 = true;
+            this.showAlert5 = true;
+            this.showAlert6 = true;
+            this.showAlert7 = true;
+            this.showAlert8 = true;
+        }
+    },
+
+};
+</script>
