3459359c841115d9191d5ebfb62498209181ab84
Jared <jared03050@gmail.com>
2021-10-29 00:36:27 +0800

    forming

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,90 @@
 <template>
     <section class="page page--ui-textbox">
 
-        <div class="page__examples">
+        <form action='/' method="post" onsubmit="return validate()">
 
-
-            <h4 class="page__demo-title">With validation: required</h4>
 
             <ui-textbox
+                icon="person_add"
                 autocomplete="off"
-                error="This field is required"
-                help="If you have multiple names, enter the one you prefer"
-                label="Name"
-                placeholder="Enter your name"
+                error="4 - 20碼"
+                help="4 - 20碼"
+                label="帳號"
+                placeholder="輸入帳號"
                 required
 
-                :invalid="textbox10Touched && textbox10.length === 0"
-                @touch="textbox10Touched = true"
+                :invalid="textAcTouched && textAc.length === 0"
+                @touch="textAcTouched = true"
 
-                v-model="textbox10"
+                v-model="textAc"
+
+                enforce-maxlength
+                :maxlength="20"
+
             ></ui-textbox>
 
-            <h4 class="page__demo-title">With validation and counter (max length)</h4>
+            <ui-textbox
+                icon="password"
+                autocomplete="off"
+                error="6 - 12碼"
+                help="6 - 12碼"
+                label="密碼"
+                placeholder="輸入密碼"
+                required
+
+                :invalid="textPwTouched && textPw.length === 0"
+                @touch="textPwTouched = true"
+
+                v-model="textPw"
+
+                enforce-maxlength
+                :maxlength="12"
+            ></ui-textbox>
 
             <ui-textbox
-                error="The username may not be more than 16 characters"
-                help="Pick a unique username not more than 16 characters"
+                error="名稱必填，不可空白與特殊符號。"
+                help="輸入名稱，不可空白與特殊符號。"
                 icon="person"
                 label="Username"
-                placeholder="Enter a username"
+                placeholder="輸入名稱"
+                required
 
                 :maxlength="16"
-                :invalid="textbox11.length > 16"
+                :invalid="userName.length > 16"
 
-                v-model="textbox11"
+                v-model="userName"
             ></ui-textbox>
 
-            <h4 class="page__demo-title">Type: number, min: 0, max: 99</h4>
+            <ui-select
+                icon="favorite"
+                label="性別"
+                placeholder="請選擇性別"
+                v-model="gender"
 
-            <ui-textbox
-                help="The ideal number of cats a person should own, minimum 0, maximum 99"
-                label="Number of Cats"
-                placeholder="Enter number of cats"
-                type="number"
+                :options="genderOptions"
+            ></ui-select>
 
-                :min="0"
-                :max="99"
-
-                v-model.number="textbox12"
+             <ui-textbox
+                help="If you have multiple email addresses, enter the one you use the most"
+                icon="mail"
+                label="Email address"
+                placeholder="Enter your email address"
+                type="email"
+                v-model="emailAddress"
             ></ui-textbox>
 
-            <h4 class="page__demo-title">Multi-line (textarea)</h4>
+            <!-- <button type="submit"> submit!</button> -->
 
-            <ui-textbox
-                enforce-maxlength
-                help="Maximum 256 characters"
-                label="Short bio"
-                placeholder="Introduce yourself in a few words"
 
-                :multi-line="true"
-                :maxlength="256"
 
-                v-model="textbox13"
-            ></ui-textbox>
+            <ui-button
+                color="primary"
+                @click="submit"
+                >送出
+            </ui-button>
 
-            <h4 class="page__demo-title">Multi-line (textarea) with floating label</h4>
-
-            <ui-textbox
-                enforce-maxlength
-                floating-label
-                help="Maximum 256 characters"
-                label="Short bio"
-                placeholder="Introduce yourself in a few words"
-
-                :multi-line="true"
-                :maxlength="256"
-
-                v-model="textbox14"
-            ></ui-textbox>
-
-            <h4 class="page__demo-title">Multi-line (textarea) with icon</h4>
-
-            <ui-textbox
-                enforce-maxlength
-                help="Maximum 256 characters"
-                icon="face"
-                label="Short bio"
-                placeholder="Introduce yourself in a few words"
-
-                :multi-line="true"
-                :maxlength="256"
-
-                v-model="textbox15"
-            ></ui-textbox>
-        </div>
+            
+        </form>
 
 
     </section>
@@ -103,23 +95,32 @@
 export default {
     data() {
         return {
-            textbox1: null,
-            textbox2: '',
-            textbox3: '',
-            textbox4: 'John Doe',
-            textbox5: 'Jane Doe',
-            textbox6: '',
-            textbox7: '',
-            textbox8: '',
-            textbox9: '',
-            textbox10: '',
-            textbox10Touched: false,
-            textbox11: '',
-            textbox12: 0,
-            textbox13: '',
-            textbox14: '',
-            textbox15: 'My name is Jane Doe...'
+
+            textAc:'',
+            textAcTouched: false,
+
+            textPw:'',
+            textPwTouched: false,
+           
+            userName:'', 
+
+            gender:'',
+            genderOptions:['男','女','/'],
+
+            emailAddress:'',
+
         };
+    },
+
+    methods:{
+        submit(){
+            console.log('btn')
+        },
+
+        validate(){
+            alert('t');
+            return false
+        }
     },
 
 };
